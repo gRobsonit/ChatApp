a64415bc83a91688473fa302a894f5e88abdf1d3
Robin Imhof <imhof.robin@gmx.ch>
2023-05-08 16:02:37 +0200

    Erste Änderungen

---
+++ script.js
@@ -0,0 +1,31 @@
+let messages = [
+    {
+        author: "Ding",
+        message: "Hallo zusammen"
+    },
+    {
+        author: "Dong",
+        message: "Hallo zurück"
+    }
+]
+
+function updateMessages() {
+    text = document.getElementById("messages").value
+    username = document.getElementById("username").value
+    if (username == "" || text == "") {
+        return
+
+comments.push({
+    text: text,
+    
+
+})
+
+}
+
+
+function submitMessage() {
+
+}
+
+document.getElementById("submitButton").onclick = submitMessage;
