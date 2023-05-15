1ee01645a46654c3fc6f75fd8de907d415eb85e6
elgithu <elias.latt@students.gymneufeld.ch>
2023-05-15 16:04:11 +0200

    Datenbank Verbindung

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+import{ initializeApp }from"https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+import{ getFirestore, orderBy, query, collection, doc, setDoc, onSnapshot,addDoc, Timestamp }from"https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+
+const firebaseConfig ={ 
+    apiKey:"AIzaSyAQ9pCLKploVByyScEYnj3yMsoNFPIt14g",            
+    authDomain:"ffinfchat.firebaseapp.com",            
+    projectId:"ffinfchat",           
+    storageBucket:"ffinfchat.appspot.com",            
+    messagingSenderId:"787327787927",            
+    appId:"1:787327787927:web:a63c242f4ba509f53c1ad2"
+};
+
 let messages = [
     {
         author: "Ding",
@@ -36,5 +48,20 @@ function submitMessage() {
         
         updateMessages()
     
+        
 }
+const app =initializeApp(firebaseConfig);
+const db =getFirestore();
+// Eine Anfrage definieren
+const q =query(collection(db,"messages"),orderBy("timestamp"));
+
+const unsubscribe =onSnapshot(q,(querySnapshot)=>{
+    const messages =[];            
+    querySnapshot.forEach((doc)=>{                
+        messages.push(doc.data());
+    
+    });
+    updateComments(messages)
+});
+
 
